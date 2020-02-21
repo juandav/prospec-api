506891a2531ad42def4dd8c397b414054de67445
juandav <e.juandav@gmail.com>
2020-02-21 11:07:24 -0500

    feat(): now you can check the backend version

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,4 +5,8 @@ export class AppService {
   getHello(): string {
     return 'Hello World!';
   }
+
+  getProjectVersion(): string {
+    return `${process.env.npm_package_name} v${process.env.npm_package_version}`;
+  }
 }
